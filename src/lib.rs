bed59666f98dfcb83a06fa40891417bbabc9cd8b
William <gopherx@protonmail.ch>
2022-07-31 18:11:55 +0800

    feat: implement calling schedule (#3)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
-use futures::StreamExt;
-use worker::{
-    console_log, event, Context, Date, Env, Request, Response, Result, Router, WebSocket,
-    WebSocketPair, WebsocketEvent,
-};
+mod session;
+mod state;
 mod utils;
+
+use session::Session;
+use state::State;
+use worker::{
+    console_debug, event, Context, Date, Env, Request, Response, Result, RouteContext, Router,
+    WebSocket, WebSocketPair,
+};
 
 #[event(fetch, respond_with_errors)]
 pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
@@ -12,7 +16,7 @@ pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
 
     let router = Router::new();
     router
-        .on_async("/signal", |req, _ctx| async move {
+        .on_async("/signal", |req, ctx| async move {
             // For WebSocket connection flow, see: https://www.wallarm.com/what/a-simple-explanation-of-what-a-websocket-is#:~:text=In%20WebSocket%2C%20communication%20occurs%20at,party%20to%20terminate%20the%20connection.
             if !req
                 .headers()
@@ -26,11 +30,11 @@ pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
             let WebSocketPair { client, server } = WebSocketPair::new()?;
 
             server.accept()?;
-            wasm_bindgen_futures::spawn_local(handle_websocket(server));
+            wasm_bindgen_futures::spawn_local(handle_websocket(server, ctx));
 
             Response::from_websocket(client)
         })
-        .or_else_any_method("/", |_, _| {
+        .or_else_any_method_async("/", |_, _| async {
             Response::ok("Welcome to Hangout, a private person to person calling service.")
         })
         .run(req, env)
@@ -38,19 +42,24 @@ pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
 }
 
 /// A WebSocket server handler.
-async fn handle_websocket(ws: WebSocket) {
-    let mut event_stream = ws.events().expect("could not open stream");
-
-    while let Some(event) = event_stream.next().await {
-        match event.expect("received error in websocket") {
-            WebsocketEvent::Message(msg) => console_log!("{:#?}", msg),
-            WebsocketEvent::Close(code) => console_log!("Closed: {:#?}", code),
-        }
-    }
+async fn handle_websocket(ws: WebSocket, ctx: RouteContext<()>) {
+    let upstash_redis_url = ctx
+        .secret("UPSTASH_REDIS_URL")
+        .expect("expect UPSTASH_REDIS_URL");
+    let upstash_redis_token = ctx
+        .secret("UPSTASH_REDIS_TOKEN")
+        .expect("expect UPSTASH_REDIS_TOKEN");
+    // Initiates state.
+    let state = State::new(
+        &upstash_redis_url.to_string(),
+        &upstash_redis_token.to_string(),
+    );
+    let session = Session::new(ws, state);
+    session.start().await;
 }
 
 fn log_request(req: &Request) {
-    console_log!(
+    console_debug!(
         "{} - [{}], located at: {:?}, within: {}",
         Date::now().to_string(),
         req.path(),
---
+++ src/session.rs
@@ -0,0 +1,210 @@
+use crate::state::{Response as StateResponse, Result as StateResult, State};
+use futures::StreamExt;
+use futures_channel::mpsc::{self, Receiver, Sender};
+use std::time::Duration;
+use worker::{console_debug, console_error, console_log, Delay, WebSocket, WebsocketEvent};
+
+#[derive(Debug)]
+pub(crate) struct Session {
+    websocket: WebSocket,
+    state: State,
+
+    signal_sender: Sender<()>,
+    signal_receiver: Receiver<()>,
+}
+
+/// A session registry.
+#[derive(Debug, Clone)]
+struct Registry {
+    /// The passphrase key to be set on Redis.
+    passphrase_key: String,
+    /// Sender channel key to be set on Redis.
+    send_channel_key: String,
+    /// Receiver channel key to be set on Redis.
+    receive_channel_key: String,
+}
+
+#[derive(Debug)]
+enum Role {
+    Caller,
+    Callee,
+}
+
+impl Session {
+    /// Creates a new session.
+    pub(crate) fn new(websocket: WebSocket, state: State) -> Session {
+        let (tx, rx) = mpsc::channel(0);
+        Session {
+            websocket,
+            state,
+            signal_sender: tx,
+            signal_receiver: rx,
+        }
+    }
+
+    pub(crate) async fn start(mut self) {
+        let mut registry = None;
+
+        // Read the first message to get passphrase.
+        let mut event_stream = self.websocket.events().expect("could not open stream");
+        let first_event = event_stream.next().await.expect("expect first message");
+        match first_event.expect("received error in websocket") {
+            WebsocketEvent::Message(msg) => {
+                // Get the passphrase as text.
+                let passphrase = msg.text().expect("expect a passphrase");
+                console_debug!("got passphrase: {:#?}", passphrase);
+
+                // Try to set this passphrase as key in Redis.
+                match self
+                    .state
+                    .set_nx(&Registry::passphrase_key(&passphrase))
+                    .await
+                {
+                    StateResponse::Result(result) => {
+                        console_debug!("got set result: {:#?}", result);
+
+                        // Get the role.
+                        // Response to client according to result.
+                        let role = match result {
+                            StateResult::Str(value) if value.eq("OK") => {
+                                // The passphrase doesn't exist before.
+                                console_debug!("this is caller");
+                                self.websocket.send_with_str("1").unwrap();
+                                Role::Caller
+                            }
+                            StateResult::Null => {
+                                // The passphrase already exists.
+                                console_debug!("this is callee");
+                                self.websocket.send_with_str("0").unwrap();
+                                Role::Callee
+                            }
+                            _ => {
+                                console_debug!("unknown result: {:#?}", result);
+                                return;
+                            }
+                        };
+                        registry = Some(Registry::new(passphrase, role));
+                    }
+                    StateResponse::Error(error) => {
+                        console_error!("could not execute set command on state: {}", error);
+                        // Signal subscription task to exit.
+                        self.signal_sender.close_channel();
+                        return;
+                    }
+                }
+
+                // Once set the passphrase, subscribe to the other party's state channel immediately.
+                wasm_bindgen_futures::spawn_local(Self::subscribe(
+                    self.state.clone(),
+                    self.websocket.clone(),
+                    registry.clone().expect("expect a registry"),
+                    self.signal_receiver,
+                ));
+            }
+            WebsocketEvent::Close(_) => console_log!("WebSocket connection closed"),
+        }
+
+        // Forward client messages to sate channel.
+        while let Some(event) = event_stream.next().await {
+            match event.expect("received error in websocket") {
+                WebsocketEvent::Message(msg) => {
+                    console_debug!("received message: {:#?}", msg);
+
+                    if let Some(content) = msg.text() {
+                        let channel_key = &registry
+                            .as_ref()
+                            .expect("expect a registry")
+                            .send_channel_key;
+                        // Send message to state channel.
+                        let response = self.state.send(channel_key, &content).await;
+                        if let StateResponse::Error(error) = response {
+                            console_error!("failed to send message to state channel: {}", error);
+
+                            // Signal subscription task to exit.
+                            self.signal_sender.close_channel();
+                            return;
+                        }
+                        console_debug!("a message is forwarded to state channel");
+                    }
+                }
+                WebsocketEvent::Close(_) => {
+                    console_log!("WebSocket connection closed");
+
+                    // Signal subscription task to exit.
+                    self.signal_sender.close_channel();
+                    return;
+                }
+            }
+        }
+    }
+
+    async fn subscribe(
+        state: State,
+        websocket: WebSocket,
+        registry: Registry,
+        mut rx: Receiver<()>,
+    ) {
+        loop {
+            let response = state.receive(&registry.receive_channel_key).await;
+            match response {
+                StateResponse::Error(error) => {
+                    console_error!("error occurred when receive from state channel: {}", error);
+                    return;
+                }
+                StateResponse::Result(result) => match result {
+                    StateResult::Str(msg) => websocket.send_with_str(msg).unwrap(),
+                    StateResult::Null => {
+                        // Sleep for 1000 ms.
+                        Delay::from(Duration::from_millis(1000)).await;
+                    }
+                    _ => {}
+                },
+            }
+
+            // Exit subscription after parent task exists.
+            if let Ok(None) = rx.try_next() {
+                // Delete the passphrase stored in Redis during session.
+                let keys = [
+                    registry.passphrase_key.as_str(),
+                    registry.send_channel_key.as_str(),
+                ];
+                state.del_keys(&keys).await;
+                break;
+            }
+        }
+    }
+}
+
+impl Registry {
+    fn new(passphrase: String, role: Role) -> Registry {
+        // A peer sends on its onw channel, receives on the other party's channel.
+        let (send_channel_key, receive_channel_key) = match role {
+            Role::Caller => (
+                Self::caller_channel_key(&passphrase),
+                Self::callee_channel_key(&passphrase),
+            ),
+            Role::Callee => (
+                Self::callee_channel_key(&passphrase),
+                Self::caller_channel_key(&passphrase),
+            ),
+        };
+        let passphrase_key = Self::passphrase_key(&passphrase);
+        Registry {
+            passphrase_key,
+            send_channel_key,
+            receive_channel_key,
+        }
+    }
+
+    fn passphrase_key(passphrase: &str) -> String {
+        format!("passphrase:{}", passphrase)
+    }
+
+    fn caller_channel_key(passphrase: &str) -> String {
+        format!("channel:caller:{}", passphrase)
+    }
+
+    fn callee_channel_key(passphrase: &str) -> String {
+        format!("channel:callee:{}", passphrase)
+    }
+}
